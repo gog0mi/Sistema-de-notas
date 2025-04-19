305b6b7c0f73d6bbdc2f09ffafe7f55509cbff73
gog0mi <geraldomisagel@gmail.com>
2025-04-18 21:38:06 -0500

    Conexion con mysql

--- backend/drf_mysql/urls.py
+++ backend/drf_mysql/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for def_mysql project.
+URL configuration for drf_mysql project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
